52ed905c7bed7004cf9f6430ed7969630d245939
Nastuki DAIGO <3so.d.na@gmail.com>
2023-09-25 18:28:07 +0900

    add: アイコンの設定

--- src/resource.ts
+++ src/resource.ts
@@ -3,7 +3,8 @@ export type CommentType = {
   author: string;
   iconUrl: string;
   date: Date;
-  isAgainstManners: boolean
+  isAgainstManners: boolean;
+  reply?: CommentType;
 };
 
 export function getComments(): CommentType[] {
@@ -11,31 +12,44 @@ export function getComments(): CommentType[] {
     {
       body: "適切なコメント1",
       author: "testName",
-      iconUrl: "./logo.svg",
+      iconUrl: "./scratch_manner_course_icon_50px.png",
       date: new Date("2023-08-01"),
-      isAgainstManners: false
+      isAgainstManners: false,
     },
     {
       body: "適切なコメント2",
       author: "2ndName",
-      iconUrl: "./logo.svg",
+      iconUrl: "./scratch_manner_course_icon_50px.png",
       date: new Date("2023-08-02"),
-      isAgainstManners: false
+      isAgainstManners: false,
     },
     {
       body: "不適切なコメント1",
       author: "3rdName",
-      iconUrl: "./logo.svg",
+      iconUrl: "./scratch_manner_course_icon_50px.png",
       date: new Date("2023-08-03"),
-      isAgainstManners: true
+      isAgainstManners: true,
     },
     {
       body: "不適切なコメント2",
       author: "4thName",
-      iconUrl: "./logo.svg",
+      iconUrl: "./scratch_manner_course_icon_50px.png",
       date: new Date("2023-08-04"),
-      isAgainstManners: true
+      isAgainstManners: true,
     },
-  ]
-
+    {
+      body: "適切なコメント3 (返信つき)",
+      author: "5thName",
+      iconUrl: "./scratch_manner_course_icon_50px.png",
+      date: new Date("2023-08-05"),
+      isAgainstManners: false,
+      reply: {
+        body: "適切な返信1",
+        author: "6thName",
+        date: new Date("2023-08-10"),
+        isAgainstManners: false,
+        iconUrl: "./scratch_manner_course_icon_50px.png",
+      },
+    },
+  ];
 }
